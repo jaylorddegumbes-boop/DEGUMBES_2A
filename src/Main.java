855bc77e4f02c139192e419adb0fefd70dece33c
jaylorddegumbes-boop <jaylorddegumbes@gmail.com>
2026-08-26 09:04:17 +0800

    initialize commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import  java.util.Scanner;
+public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("===JAVA ACTIVITY PORTFOLIO===");
+    System.out.println("==Name: lord");
+    System.out.println("==Section/Year: BSIT 2A");
+    System.out.println("=======CONTENTS=======");
+    System.out.println("1. Activity 1");
+    System.out.println("2. Activity 2");
+    System.out.println("==============================");
+    System.out.print("Enter Selection: ");
+    int select = sc.nextInt();
+
+    if(select == 1 ){
+        Activity1 act1 = new Activity1();
+        act1.greetUser();
+    }else if (select == 2 ){
+
+    }else{
+        System.out.println("Selection not Found");
+    }
+
+
+
+}
